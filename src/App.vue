27ab44943ed8064595eaffaf8bc3e27eecd04f21
Ravi <ravisankkaran.i@medyaan.com>
2025-08-12 14:49:08 +0530

    updated nav

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,19 @@
       <!-- nav bar title -->
       <a class="navbar-brand" href="#">KELLY</a>
       <!-- mobile view toggle button -->
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">  
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <!-- nav-bar container -->
           <li class="nav-item">
@@ -24,35 +32,36 @@
             <router-link class="nav-link" to="/service">Services</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/portfolio">Portfolio</router-link>
+            <router-link class="nav-link" to="/portfolio"
+              >Portfolio</router-link
+            >
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/contact">Contact</router-link>
           </li>
           <!-- social media links -->
-          <li class="nav-item social-links">
+          <!-- <li class="nav-item social-links">
             <a class="nav-link" href="#"><i class="fab fa-twitter"></i></a>
             <a class="nav-link" href="#"><i class="fab fa-facebook"></i></a>
             <a class="nav-link" href="#"><i class="fab fa-instagram"></i></a>
             <a class="nav-link" href="#"><i class="fab fa-linkedin"></i></a>
-          </li>
+          </li> -->
         </ul>
       </div>
     </nav>
     <!-- rout view  -->
-    <router-view/>
+    <router-view />
     <!-- footer -->
     <copyright />
   </div>
 </template>
 
 <script>
-import copyright from './components/CopyrightPage.vue'
+import copyright from "./components/CopyrightPage.vue";
 export default {
-  name: 'App',
-  components:{
-    copyright
-        },
-}
+  name: "App",
+  components: {
+    copyright,
+  },
+};
 </script>
-
